df0fb7f4736fef7dcf2f6abdc960f24455d50571
hellenilda <hellen95limaaraujo@gmail.com>
2024-06-17 20:35:00 -0300

    Correção: Animações do Player

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,21 +13,25 @@ func _physics_process(delta):
 	# Adiciona a gravidade
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		animacao.play("jump") # Quando o player pula, ativa a animação "jump"
-	else:
-		# Configurações de pulos do player
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor(): # ui_accept = barra de espaço
-			velocity.y = JUMP_FORCE
 
-		# Obtém a direção e controla o movimento/desaceleração
-		var direction = Input.get_axis("ui_left", "ui_right")
-		
-		if direction:
-			velocity.x = direction * SPEED
-			animacao.play("run") # se velocidade != 0: a animação "run" é ativada
-			animacao.scale.x = direction # inverte o player horizontalmente quando ele mudar de direção
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-			animacao.play("idle") # quando a velocidade = 0: volta para a animação "idle"
+	# Configurações de pulos do player
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor(): # ui_accept = barra de espaço
+		velocity.y = JUMP_FORCE
+
+	# Obtém a direção e controla o movimento/desaceleração
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		animacao.scale.x = direction # inverte o player horizontalmente quando ele mudar de direção
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# Ativa a animação apropriada
+	if not is_on_floor():
+		animacao.play("jump") # Ativa a animação "jump" quando o player está no ar
+	elif direction:
+		animacao.play("run") # Ativa a animação "run" quando o player está se movendo
+	else:
+		animacao.play("idle") # Ativa a animação "idle" quando o player está parado
 
 	move_and_slide()
